ee5c6c4a6e317fc91be569a74fa8b1cfb00eb092
Tlou <tlou.maponya@umuzi.org>
2020-01-15 14:34:13 +0200

    Updated Main Program

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,16 +7,20 @@ public class Main {
         Scanner input = new Scanner(System.in);
         PasswordChecker test = new PasswordChecker();
         String password = "";
-
         System.out.println("Please enter your password: ");
         System.out.println("(Password should be between greater than 8 characters");
         System.out.println("and should consist of at least 1 Special character, 1 uppercase, 1 lowercase and a number) \n");
-
-        do {
         password = input.nextLine();
-    }
-    while (!test.isPasswordValid(password));
-        System.out.println(test.isPasswordValid(password));
+
+        try {
+            System.out.println("passwordIsOk: " + test.passwordIsOk(password));
+            System.out.println("isPasswordValid: " + test.isPasswordValid(password));
+        }
+        catch (IllegalArgumentException e){
+            e.printStackTrace();
+        }
+
 
     }
+
 }
