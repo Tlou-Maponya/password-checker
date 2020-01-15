ec33c20e49246f7cd6c8bb43bff0d73f077d526d
Tlou <tlou.maponya@umuzi.org>
2020-01-15 10:29:28 +0200

    updated main progam

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,24 +1,22 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Please enter name: ");
-        Scanner input2 = new Scanner(System.in);
-        String name = input2.nextLine();
+    public static void main(String[] args) { ;
 
         //input password from user
+        Scanner input = new Scanner(System.in);
+        PasswordChecker test = new PasswordChecker();
+        String password = "";
+
         System.out.println("Please enter your password: ");
         System.out.println("(Password should be between greater than 8 characters");
-        System.out.println("and should consist of atleast 1 Special character, 1 uppercase, 1 lowercase and a number) ");
-        Scanner input = new Scanner(System.in);
-        String password = input.nextLine();
+        System.out.println("and should consist of at least 1 Special character, 1 uppercase, 1 lowercase and a number) \n");
 
-        PasswordChecker test = new PasswordChecker();
-        boolean myPass = test.isPasswordValid(password);
-
-        System.out.println(myPass);
-        //System.out.println(password);
-
+        do {
+        password = input.nextLine();
+    }
+    while (!test.isPasswordValid(password));
+        System.out.println(test.isPasswordValid(password));
 
     }
 }
